17631203acb9fd087f121d96a0fe814a23991fc3
Osama <abdulhaihafeez@gmail.com>
2024-03-11 13:21:47 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Welcome Prompt On Web Page
+let userName = prompt("What is Your Name");
+alert("Welcom " + userName);
+console.log(userName);
+
+// User's Added Numbers Are Shown Here.
+let num1 = prompt("Enter Any First Num");
+let num2 = prompt("Enter Any Second Num");
+
+// Strings Convert into Numbers Here.
+num1 = Number(num1);
+num2 = Number(num2);
+console.log("num1: ", num1);
+console.log("num2: ", num2);
+
+// User's Numbers Are Addition & Prompted Here.
+let sum = num1 + num2;
+alert("Addition " + sum);
+console.log("Addition " + sum);
+
+// User's Numbers Are Subtraction & Prompted Here.
+let minus = num1 - num2;
+alert("Subtraction " + minus);
+console.log("Subtraction " + minus);
+
+// User's Numbers Are Multiplication & Prompted Here.
+let multiply = num1 * num2;
+alert("Multiplication " + num1 * num2);
+console.log("Multiplication " + num1 * num2);
+
+// User's Numbers Are Division & Prompted Here.
+let divide = num1 / num2;
+alert("Division " + num1 / num2);
+console.log("Division " + num1 / num2);
